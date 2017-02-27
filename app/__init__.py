7ebc03072961b7c633cb43cc64ef5793b5376547
Odaine <odaine.forbes@yahoo.com>
2017-02-27 03:39:42 +0000

    step 6

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
+app.config['SECRET_KEY'] = "sup3rk3y"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:lab5@localhost/lab5"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
